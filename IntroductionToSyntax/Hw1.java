e4834e26de24cff8a24ca44a209776f5cbe7405a
nekootrap <polatihonova933@gmail.com>
2026-02-23 14:08:39 +0300

    Work1

---
+++ IntroductionToSyntax/Hw1.java
@@ -0,0 +1,63 @@
+public class Hw1 {
+
+    public static void main(String[] args) {
+        // Task 1
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println(dog + " " + cat + " " + paper);
+        
+        // Task 2
+        dog += 4;
+        cat += 4;
+        paper += 4;
+        System.out.println(dog + " " + cat + " " + paper);
+
+        // Task 3
+        dog -= 3.5;
+        cat -= 1.6;
+        paper -= 7639;
+        System.out.println(dog + " " + cat + " " + paper);
+
+        // Task 4
+        var friend = 19;
+        System.out.println(friend);
+
+        friend *= 2;
+        System.out.println(friend);
+
+        friend /= 7;
+        System.out.println(friend);
+
+        // Task 5
+        var frog = 3.5;
+        System.out.println(frog);
+
+        frog *= 10;
+        System.out.println(frog);
+
+        frog /= 3.5;
+        System.out.println(frog);
+
+        frog += 4;
+        System.out.println(frog);
+
+        // Task 6
+        var boxer1 = 78.2;
+        var boxer2 = 82.7;
+        System.out.println(boxer1 + boxer2);
+        System.out.println(boxer2 - boxer1);
+
+        // Task 7
+        System.out.println(boxer2 % boxer1);
+
+        // Task 8
+        var workers = 640 / 8;
+        System.out.println("Всего работников в компании — " + workers +  " человек");
+        System.out.println("Если в компании работает " + (workers + 94) + " человек, то всего " + ((workers + 94) * 8) + " часов работы может быть поделено между сотрудниками");
+
+    }
+
+}
+
+
